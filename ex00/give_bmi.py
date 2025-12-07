98060c6701729e7fad054abe57b54fcfc80094a1
MosesAwad <mawad@student.42abudhabi.ae>
2025-12-08 02:22:34 +0400

    Added ex00 implementation

---
+++ ex00/give_bmi.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
+    try:
+        assert len(height) == len(weight), \
+            "height and weight lists not of equal length"
+
+        assert isinstance(height, list) and isinstance(weight, list), \
+            "height and weight must be of type list"
+
+        for h, w in zip(height, weight):
+            assert isinstance(h, (int, float)) and \
+                isinstance(w, (int, float)), \
+                "height and weight must only contain either \
+                    float or int values"
+            assert h > 0 and w > 0, \
+                "height and weight must have only positive values"
+
+        h = np.array(height)
+        w = np.array(weight)
+
+        return (w / (h ** 2)).tolist()
+    except AssertionError as e:
+        print("AssertionError: ", e)
+    except Exception as e:
+        print("Exception: ", e)
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    try:
+        assert isinstance(bmi, list), "bmi must be of type list"
+        assert isinstance(limit, int), "limit must be an integer value"
+        for b in bmi:
+            assert isinstance(b, (float, int)), \
+                "bmi must only contain either float or int values"
+            assert bmi > 0, "cannot have negative bmi values"
+
+        return (np.array(bmi) > limit).tolist()
+    except AssertionError as e:
+        print("AssertionError: ", e)
+    except Exception as e:
+        print("Exception: ", e)
+
+
+if __name__ == "__main__":
+    # height = [189, 178, 163, 190, 172]
+    # weight = [92, 81, 72, 98, 93]
+
+    # print(give_bmi(height, weight))
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+
+"""
+    def give_bmi(height: list[int | float], weight: list[int | float])
+        -> list[int | float]:
+        bmi = []
+
+        for i in range(len(height)):
+            bmi.append(weight[i] / (height[i] * height[i]))
+
+        return bmi
+"""
